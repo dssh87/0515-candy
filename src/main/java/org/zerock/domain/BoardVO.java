bfd9b2a8d052be83a7aea0b239e4147d3badc9cd
poohson777 <poohson777@gmail.com>
2018-05-16 19:19:21 +0900

    checkNew...modify....

--- src/main/java/org/zerock/domain/BoardVO.java
+++ src/main/java/org/zerock/domain/BoardVO.java
@@ -7,8 +7,17 @@ import lombok.Data;
 @Data
 public class BoardVO {
 
-	private Integer bno, newIcon;
+	private Integer bno;
 	private String title, content, writer;
 	private Date regdate, updatedate;
 	
+	private static final Long ADAY = 1000*60*60*24L;
+	
+	public boolean checkNew() {
+		
+		long gap = new Date().getTime() - regdate.getTime();
+		
+		return gap < ADAY;
+		
+	}
 }
