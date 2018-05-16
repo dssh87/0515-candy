b2cfe125bd42c6d01dc20132ab15cfbaa46cb738
poohson777 <poohson777@gmail.com>
2018-05-16 10:44:50 +0900

    BoardController

--- src/main/java/org/zerock/controller/BoardController.java
+++ src/main/java/org/zerock/controller/BoardController.java
@@ -1,15 +1,11 @@
 package org.zerock.controller;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.zerock.domain.BoardVO;
 import org.zerock.domain.Criteria;
 import org.zerock.domain.PageMaker;
 import org.zerock.service.BoardService;
